39c44d189a15e0e24d860a770423db2a0e232f79
Rahulruben Mule <mulerahulruben@gmail.com>
2024-11-09 16:41:42 +0530

    Addition of the Header component with Logo and Nav items

--- App.js
+++ App.js
@@ -1,7 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Container = () => <div id="container">This is a container</div>;
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img
+        className="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyqFrCVksYvoEmXEuleFofdLdIYias2JsSETf5bafjd0WRUZ34MsmqUxPsNNtzGIKwLmA&usqp=CAU"
+        alt="Food delivery App"
+      />
+    </div>
+  );
+};
+
+const NavContainer = () => {
+  return (
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Logo />
+      <NavContainer />
+    </div>
+  );
+};
+
+const AppContainer = () => (
+  <div id="app">
+    <Header />
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Container />);
+root.render(<AppContainer />);
